2fa53052dbf0c0c951c0eabad912f7404b6c441e
Shurabota <shurabota2801@gmail.com>
2025-10-14 22:20:16 +0500

    first commit

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model):
-    Author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
-    title = models.CharField(max_length=100, verbose_name='Заголовок')
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
     
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержание')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Изменено')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    content = models.TextField(max_length=2000, verbose_name='Комментарий')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Пост')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f'Комментарий от {self.author} к "{self.post.title}"'
