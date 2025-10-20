390de4d45f3f44f2d11ab916aca6fd5b0e50c842
Shurabota <shurabota2801@gmail.com>
2025-10-20 21:27:09 +0500

    first commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post, Comment, Category
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm, CommentForm
+from django.urls import reverse
 
-# Create your views here.
+def post_list(request):
+    pass
